933c8bfd6789d9be2c6aa0a535ce50817cf9aade
Hitesh Mankar <48584688+hiteshmankar94@users.noreply.github.com>
2022-10-07 22:41:07 +0530

    Update Assignment1_7.py

--- Assignment1/Assignment1_7.py
+++ Assignment1/Assignment1_7.py
@@ -1,3 +1,5 @@
+#Write a program which contains one function that accept one number from user and returns true if number is divisible by 5 otherwise return false.
+
 def div_test():
     ino = int(input("Enter the number:"))
     if ino % 5 == 0:
