a7d23fb213afbd6f486a05edff3a0748c8e708b0
Hitesh Mankar <48584688+hiteshmankar94@users.noreply.github.com>
2022-10-07 22:36:17 +0530

    Update Assignment1_2.py

--- Assignment1/Assignment1_2.py
+++ Assignment1/Assignment1_2.py
@@ -1,3 +1,5 @@
+#  Write a program which contains one function named as ChkNum() which accept one
+#  parameter as number. If number is even then it should display “Even number” otherwise  display “Odd number” on console.
 
 def ChkNum(num):
     if num%2 == 0:
